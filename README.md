5963323084fc44e15ceccc32a723bbd26397a015
Chandramohan Mani <chandramohan.mani@pluribusnetworks.com>
2018-02-02 19:13:53 -0800

    add binary search

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# array-sort
+# Sort Array
+
+# search a sorted array by repeatedly splitting the array in half using Binary Search
+# run the sort.js and binary-search.js togather
---
+++ binary-search.js
@@ -0,0 +1,18 @@
+let arr = [255, 266, 322, 453, 648, 657, 750, 861, 878, 963];
+console.log(arr);
+let rand = Math.round(Math.random() * 20);
+console.log("rand ", rand);
+let target = arr[rand];
+console.log(target);
+
+function bsearch(arr, target, start, end) {
+  let mid = Math.round((start + end)/2);
+  console.log(target, start, end, mid);
+
+  if (target == arr[mid]) return [arr[mid], mid];
+  if (target < arr[mid]) return bsearch(arr, target, start, mid);
+  if (target > arr[mid]) return bsearch(arr, target, mid, end);
+}
+
+let result = bsearch(arr, target, 0, (arr.length - 1));
+console.log(result);
